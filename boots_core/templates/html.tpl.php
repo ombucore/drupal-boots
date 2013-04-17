2150b55f0e61ee8bf62a22baa6837f764d2241e0
Jon Glick <jonathanpglick@gmail.com>
2013-04-17 12:28:39 -0700

    Remove no-js class from conditional classes

--- boots_core/templates/html.tpl.php
+++ boots_core/templates/html.tpl.php
@@ -42,10 +42,10 @@
  */
 ?>
 <!doctype html>
-<!--[if lt IE 7]>      <html lang="<?php print $language->language; ?>" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html lang="<?php print $language->language; ?>" class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html lang="<?php print $language->language; ?>" class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html lang="<?php print $language->language; ?>" class="no-js"> <!--<![endif]-->
+<!--[if lt IE 7]>      <html lang="<?php print $language->language; ?>" class="lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="<?php print $language->language; ?>" class="lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="<?php print $language->language; ?>" class="lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="<?php print $language->language; ?>" class=""> <!--<![endif]-->
 <head>
   <title><?php print $head_title; ?></title>
   <?php print $head; ?>
