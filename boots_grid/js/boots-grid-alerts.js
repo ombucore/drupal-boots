14a354cc25cef9039dcccf2f8cd1a1f8ac90953c
Adam Page <adamfpage@gmail.com>
2015-03-20 10:02:31 -0700

    Refs #10716: Fixed alert dismiss behaviors and finessed styles for cleaner overriding in project themes.

--- boots_grid/js/boots-grid-alerts.js
+++ boots_grid/js/boots-grid-alerts.js
@@ -1,10 +1,11 @@
 (function ($) {
   Drupal.behaviors.bootsGridAlerts = {
     attach: function(context, settings) {
+
       // When the user clicks an alert's dismiss link, class it accordingly.
-      $('[data-name="console"]').find('.dismiss').on('click', function(e) {
+      $('[data-name="console"]').find('.close').on('click', function(e) {
         e.preventDefault();
-        var container = $(this).closest($(this).attr('data-selector'));
+        var container = $(this).closest('.' + $(this).attr('data-dismiss'));
         // Set the max-height of each alert block to its actual pixel height.
         // By doing this, the accompanying CSS rules will smoothly transition
         // the max-height to 0.
