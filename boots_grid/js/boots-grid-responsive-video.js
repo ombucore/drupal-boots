b9c74e68aebf20ef6d26ef6c32680b6ffba05cc1
Jon Duell <jon@jonduell.com>
2015-05-13 17:14:24 -0700

    Adds video reponsive wrapper for sections

--- boots_grid/js/boots-grid-responsive-video.js
+++ boots_grid/js/boots-grid-responsive-video.js
@@ -4,7 +4,7 @@
     attach: function(context, settings) {
     // Wrap iframe elements inside the content region.
     // TODO: Alter the rendered markup for embedded videos in Drupal.
-    $('[data-type="region"][data-name="content"] iframe', context).once('responsive-video').wrap('<div class="video-container"><div class="video-frame"></div></div>');
+    $('[data-type="region"][data-name="content"] iframe, [data-type="region"][data-name="section"] iframe', context).once('responsive-video').wrap('<div class="video-container"><div class="video-frame"></div></div>');
 
     }
   };
