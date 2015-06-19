f9b0c82039a8e9c730f8664d9c9342a98231cfd4
Jon Duell <jon@jonduell.com>
2015-06-19 09:48:13 -0700

    Adds data type to sidebar blocks

--- boots_core/templates/blocks/block--sidebar-first.tpl.php
+++ boots_core/templates/blocks/block--sidebar-first.tpl.php
@@ -42,7 +42,7 @@
  * @see template_process()
  */
 ?>
-<aside id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<aside data-type="block" id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
 <?php if ($block->subject): ?>
